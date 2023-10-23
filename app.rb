596404762bd13319c65ae89f1f2d78e6fe2da55d
lidavid662 <146143936+lidavid662@users.noreply.github.com>
2023-10-23 03:38:17 +0000

    fix float

--- app.rb
+++ app.rb
@@ -12,11 +12,18 @@ get("/square/new") do
   erb(:square)
 end
 
+get("/square/results") do
+  @number=params.fetch("number").to_s
+  @square=@number*@number
+  erb(:squarer)
+end
+
 get("/square_root/new") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:square_root)
+end
+
+get("/square_root/results") do
+  erb(:square_rootr)
 end
 
 get("/payment/new") do
